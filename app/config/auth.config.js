c173540d49f63c81cb3e936d868d67f95ecfd062
Ichsan <mihsandf759@gmail.com>
2022-12-22 12:05:34 +0700

    first commit

---
+++ app/config/auth.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  secret: "sadolorsadadsaci4dsadsa",
+};
---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const responseHelper = require("express-response-helper").helper();
+const cors = require("cors");
+const db = require("./app/models");
+
+const app = express();
+
+var corsOptions = {
+  origin: "http://localhost:8081",
+};
+
+app.use(cors(corsOptions));
+
+// attach the middleware before any route definition
+app.use(responseHelper);
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to my base project." });
+});
+
+app.get("/user", async (req, res) => {
+  const users = await db.User.findAll();
+  res.respond(users, 200);
+});
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
